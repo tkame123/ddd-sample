1b8596b0008966bf23f3212f30ece4d51458f60e
Takeshi Kameyama <tkame123@gmail.com>
2024-10-08 20:10:35 +0900

    refactor

--- app/order_api/usecase/create_order/create_order.go
+++ app/order_api/usecase/create_order/create_order.go
@@ -2,17 +2,18 @@ package create_order
 
 import (
 	"context"
+	"github.com/google/uuid"
 	"github.com/tkame123/ddd-sample/app/order_api/domain/model"
 )
 
 func (s *s) CreateOrder(ctx context.Context, items []*model.OrderItemRequest) (model.OrderID, error) {
 	order, events, err := model.NewOrder(items)
 	if err != nil {
-		return "", err
+		return uuid.Nil, err
 	}
 
 	if err := s.rep.OrderSave(ctx, order); err != nil {
-		return "", err
+		return uuid.Nil, err
 	}
 
 	s.pub.PublishMessages(ctx, events)
