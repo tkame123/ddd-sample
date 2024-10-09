83d51d32300bd73bfa0a5e7bef3c76702fca1921
Takeshi Kameyama <tkame123@gmail.com>
2024-10-09 16:02:01 +0900

    refactor transaction

--- app/order_api/adapter/database/order.go
+++ app/order_api/adapter/database/order.go
@@ -2,11 +2,9 @@ package database
 
 import (
 	"context"
-	"fmt"
 	"github.com/tkame123/ddd-sample/app/order_api/adapter/database/ent"
 	e_order "github.com/tkame123/ddd-sample/app/order_api/adapter/database/ent/order"
 	e_orderItem "github.com/tkame123/ddd-sample/app/order_api/adapter/database/ent/orderitem"
-
 	"github.com/tkame123/ddd-sample/app/order_api/domain/model"
 )
 
@@ -15,61 +13,48 @@ func (r *repo) OrderFindOne(ctx context.Context, id model.OrderID) (*model.Order
 }
 
 func (r *repo) OrderSave(ctx context.Context, order *model.Order) error {
-	tx, err := r.db.Tx(ctx)
-	if err != nil {
+	if err := r.WithTx(ctx, func(tx *ent.Tx) error {
+		err := tx.Order.Create().
+			SetID(order.OrderID()).
+			SetApprovalLimit(int64(order.ApprovalLimit())).
+			SetStatus(fromModelStatus(order.Status())).
+			OnConflictColumns("id").
+			UpdateNewValues().
+			Exec(ctx)
+		if err != nil {
+			return err
+		}
+
+		_, err = tx.OrderItem.Delete().
+			Where(e_orderItem.OrderID(order.OrderID())).
+			Exec(ctx)
+		if err != nil {
+			return err
+		}
+
+		orderItems := order.OrderItems()
+		err = tx.OrderItem.MapCreateBulk(
+			orderItems,
+			func(
+				oi *ent.OrderItemCreate, i int) {
+				item := orderItems[i]
+				oi.SetID(item.OrderItemID)
+				oi.SetOrderID(item.OrderID)
+				oi.SetItemID(item.ItemID)
+				oi.SetSortNo(int32(item.SortNo))
+				oi.SetPrice(int64(item.Price))
+				oi.SetQuantity(int32(item.Quantity))
+			},
+		).Exec(ctx)
+		if err != nil {
+			return err
+		}
+		return nil
+	}); err != nil {
 		return err
-	}
-	err = tx.Order.Create().
-		SetID(order.OrderID()).
-		SetApprovalLimit(int64(order.ApprovalLimit())).
-		SetStatus(fromModelStatus(order.Status())).
-		OnConflictColumns("id").
-		UpdateNewValues().
-		Exec(ctx)
-	if err != nil {
-		return rollback(tx, err)
-	}
-
-	_, err = tx.OrderItem.Delete().
-		Where(e_orderItem.OrderID(order.OrderID())).
-		Exec(ctx)
-	if err != nil {
-		return rollback(tx, err)
-	}
-
-	orderItems := order.OrderItems()
-	err = tx.OrderItem.MapCreateBulk(
-		orderItems,
-		func(
-			oi *ent.OrderItemCreate, i int) {
-			item := orderItems[i]
-			oi.SetID(item.OrderItemID)
-			oi.SetOrderID(item.OrderID)
-			oi.SetItemID(item.ItemID)
-			oi.SetSortNo(int32(item.SortNo))
-			oi.SetPrice(int64(item.Price))
-			oi.SetQuantity(int32(item.Quantity))
-		},
-	).Exec(ctx)
-	if err != nil {
-		return rollback(tx, err)
-	}
-
-	err = tx.Commit()
-	if err != nil {
-		return rollback(tx, err)
 	}
 
 	return nil
-}
-
-// rollback calls to tx.Rollback and wraps the given error
-// with the rollback error if occurred.
-func rollback(tx *ent.Tx, err error) error {
-	if rerr := tx.Rollback(); rerr != nil {
-		err = fmt.Errorf("%w: %v", err, rerr)
-	}
-	return err
 }
 
 func fromModelStatus(status model.OrderStatus) e_order.Status {
--- app/order_api/adapter/database/repository.go
+++ app/order_api/adapter/database/repository.go
@@ -1,6 +1,8 @@
 package database
 
 import (
+	"context"
+	"fmt"
 	"github.com/tkame123/ddd-sample/app/order_api/adapter/database/ent"
 	"github.com/tkame123/ddd-sample/app/order_api/domain/port/repository"
 )
@@ -11,4 +13,27 @@ type repo struct {
 
 func NewRepository(db *ent.Client) repository.Repository {
 	return &repo{db: db}
+}
+
+func (r *repo) WithTx(ctx context.Context, fn func(tx *ent.Tx) error) error {
+	tx, err := r.db.Tx(ctx)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if v := recover(); v != nil {
+			tx.Rollback()
+			panic(v)
+		}
+	}()
+	if err := fn(tx); err != nil {
+		if rerr := tx.Rollback(); rerr != nil {
+			err = fmt.Errorf("%w: rolling back transaction: %v", err, rerr)
+		}
+		return err
+	}
+	if err := tx.Commit(); err != nil {
+		return fmt.Errorf("committing transaction: %w", err)
+	}
+	return nil
 }
