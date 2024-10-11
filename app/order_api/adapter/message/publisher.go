27f03fd5de9f6b5f9999c6929e02eb456d18610b
Takeshi Kameyama <tkame123@gmail.com>
2024-10-11 15:35:30 +0900

    refactor sns

--- app/order_api/adapter/message/publisher.go
+++ app/order_api/adapter/message/publisher.go
@@ -2,34 +2,55 @@ package message
 
 import (
 	"context"
+	"github.com/caarlos0/env/v11"
 	"github.com/tkame123/ddd-sample/app/order_api/adapter/message/sns"
 	"github.com/tkame123/ddd-sample/app/order_api/domain/port/domain_event"
 	"github.com/tkame123/ddd-sample/lib/event"
 	"log"
 )
 
-var topicMap = map[event.Name]sns.Topic{
-	event.EventName_OrderCreated:   sns.Topic_Event_Order_OrderCreated,
-	event.EventName_OrderApproved:  sns.Topic_Event_Order_OrderrApproved,
-	event.EventName_OrderRejected:  sns.Topic_Event_Order_OrderRejected,
-	event.EventName_TicketCreated:  sns.Topic_Command_Kitchen_TicketCreate,
-	event.EventName_TicketApproved: sns.Topic_Command_Kitchen_TicketAprove,
-	event.EventName_TicketRejected: sns.Topic_Command_Kitchen_TicketReject,
-	event.EventName_CardAuthorized: sns.Topic_Command_Billing_CardAuthorize,
+type topicArn = string
+
+type arnConfig struct {
+	ArnTopicEventOrderOrderCreated      string `env:"TOPIC_ARN_EVENT_ORDER_ORDER_CREATED"`
+	ArnTopicEventOrderOrderApproved     string `env:"TOPIC_ARN_EVENT_ORDER_ORDER_APPROVED"`
+	ArnTopicEventOrderOrderRejected     string `env:"TOPIC_ARN_EVENT_ORDER_ORDER_REJECTED"`
+	ArnTopicCommandOrderOrderApprove    string `env:"TOPIC_ARN_COMMAND_ORDER_ORDER_APPROVE"`
+	ArnTopicCommandOrderOrderReject     string `env:"TOPIC_ARN_COMMAND_ORDER_ORDER_REJECT"`
+	ArnTopicCommandKitchenTicketCreate  string `env:"TOPIC_ARN_COMMAND_KITCHEN_TICKET_CREATE"`
+	ArnTopicCommandKitchenTicketApprove string `env:"TOPIC_ARN_COMMAND_KITCHEN_TICKET_APPROVE"`
+	ArnTopicCommandKitchenTicketReject  string `env:"TOPIC_ARN_COMMAND_KITCHEN_TICKET_REJECT"`
+	ArnTopicCommandBillingCardAuthorize string `env:"TOPIC_ARN_COMMAND_BILLING_CARD_AUTHORIZE"`
 }
 
 type eventPublisher struct {
-	sns sns.Actions
+	sns      sns.Actions
+	topicMap map[event.Name]topicArn
 }
 
 func NewEventPublisher(sns *sns.Actions) domain_event.Publisher {
-	return &eventPublisher{sns: *sns}
+	var arnCfg arnConfig
+	if err := env.Parse(&arnCfg); err != nil {
+		log.Fatalf("unable to parase env, %v", err)
+	}
+
+	topicMap := map[event.Name]topicArn{
+		event.EventName_OrderCreated:    arnCfg.ArnTopicEventOrderOrderCreated,
+		event.EventName_OrderApproved:   arnCfg.ArnTopicEventOrderOrderApproved,
+		event.EventName_OrderRejected:   arnCfg.ArnTopicEventOrderOrderRejected,
+		event.CommandName_TicketCreate:  arnCfg.ArnTopicCommandKitchenTicketCreate,
+		event.CommandName_TicketApprove: arnCfg.ArnTopicCommandOrderOrderApprove,
+		event.CommandName_TicketReject:  arnCfg.ArnTopicCommandOrderOrderReject,
+		event.CommandName_CardAuthorize: arnCfg.ArnTopicCommandBillingCardAuthorize,
+	}
+
+	return &eventPublisher{sns: *sns, topicMap: topicMap}
 }
 
 func (s *eventPublisher) PublishMessages(ctx context.Context, events []event.Event) {
 	for _, e := range events {
-		var topic sns.Topic
-		topic, ok := topicMap[e.Name()]
+		var topic string
+		topic, ok := s.topicMap[e.Name()]
 		if !ok {
 			log.Printf("topic not found: %s", e.Name())
 			continue
--- app/order_api/adapter/message/sns/sns.go
+++ app/order_api/adapter/message/sns/sns.go
@@ -2,44 +2,14 @@ package sns
 
 import (
 	"context"
-	"errors"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/sns"
-	"github.com/caarlos0/env/v11"
 	"log"
 )
 
-type Topic = int
-type topicArn = string
-
-const (
-	Topic_Event_Order_OrderCreated Topic = iota
-	Topic_Event_Order_OrderrApproved
-	Topic_Event_Order_OrderRejected
-	Topic_Command_Order_OrderApprove
-	Topic_Command_Order_OrderReject
-	Topic_Command_Kitchen_TicketCreate
-	Topic_Command_Kitchen_TicketAprove
-	Topic_Command_Kitchen_TicketReject
-	Topic_Command_Billing_CardAuthorize
-)
-
-type arnConfig struct {
-	ArnTopicEventOrderOrderCreated      string `env:"TOPIC_ARN_EVENT_ORDER_ORDER_CREATED"`
-	ArnTopicEventOrderOrderApproved     string `env:"TOPIC_ARN_EVENT_ORDER_ORDER_APPROVED"`
-	ArnTopicEventOrderOrderRejected     string `env:"TOPIC_ARN_EVENT_ORDER_ORDER_REJECTED"`
-	ArnTopicCommandOrderOrderApprove    string `env:"TOPIC_ARN_COMMAND_ORDER_ORDER_APPROVE"`
-	ArnTopicCommandOrderOrderReject     string `env:"TOPIC_ARN_COMMAND_ORDER_ORDER_REJECT"`
-	ArnTopicCommandKitchenTicketCreate  string `env:"TOPIC_ARN_COMMAND_KITCHEN_TICKET_CREATE"`
-	ArnTopicCommandKitchenTicketApprove string `env:"TOPIC_ARN_COMMAND_KITCHEN_TICKET_APPROVE"`
-	ArnTopicCommandKitchenTicketReject  string `env:"TOPIC_ARN_COMMAND_KITCHEN_TICKET_REJECT"`
-	ArnTopicCommandBillingCardAuthorize string `env:"TOPIC_ARN_COMMAND_BILLING_CARD_AUTHORIZE"`
-}
-
 type Actions struct {
-	Client   *sns.Client
-	topicMap map[Topic]topicArn
+	Client *sns.Client
 }
 
 func NewActions() *Actions {
@@ -48,36 +18,15 @@ func NewActions() *Actions {
 	if err != nil {
 		log.Fatalf("unable to load SDK config, %v", err)
 	}
-	var arnCfg arnConfig
-	if err := env.Parse(&arnCfg); err != nil {
-		log.Fatalf("unable to parase env, %v", err)
-	}
 
 	snsClient := sns.NewFromConfig(cfg)
-	topicMap := map[Topic]topicArn{
-		Topic_Event_Order_OrderCreated:      arnCfg.ArnTopicEventOrderOrderCreated,
-		Topic_Event_Order_OrderrApproved:    arnCfg.ArnTopicEventOrderOrderApproved,
-		Topic_Event_Order_OrderRejected:     arnCfg.ArnTopicEventOrderOrderRejected,
-		Topic_Command_Order_OrderApprove:    arnCfg.ArnTopicCommandOrderOrderApprove,
-		Topic_Command_Order_OrderReject:     arnCfg.ArnTopicCommandOrderOrderReject,
-		Topic_Command_Kitchen_TicketCreate:  arnCfg.ArnTopicCommandKitchenTicketCreate,
-		Topic_Command_Kitchen_TicketAprove:  arnCfg.ArnTopicCommandKitchenTicketApprove,
-		Topic_Command_Kitchen_TicketReject:  arnCfg.ArnTopicCommandKitchenTicketReject,
-		Topic_Command_Billing_CardAuthorize: arnCfg.ArnTopicCommandBillingCardAuthorize,
-	}
 
 	return &Actions{
-		Client:   snsClient,
-		topicMap: topicMap,
+		Client: snsClient,
 	}
 }
 
-func (a Actions) PublishMessage(ctx context.Context, topic Topic, message string) error {
-	arn, ok := a.topicMap[topic]
-	if !ok {
-		return errors.New("topic not found")
-	}
-
+func (a Actions) PublishMessage(ctx context.Context, arn string, message string) error {
 	input := sns.PublishInput{
 		TopicArn: aws.String(arn),
 		Message:  aws.String(message),
