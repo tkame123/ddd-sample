f3b63d011b8255b06a9f65cae13486f4b613a55a
Takeshi Kameyama <tkame123@gmail.com>
2024-10-12 21:29:27 +0900

    fixed contexer saga

--- app/order_api/adapter/message/create_order_saga_context.go
+++ app/order_api/adapter/message/create_order_saga_context.go
@@ -3,50 +3,51 @@ package message
 import (
 	"context"
 	"errors"
+	"fmt"
 	"github.com/tkame123/ddd-sample/app/order_api/domain/port/domain_event"
 	"github.com/tkame123/ddd-sample/app/order_api/domain/service/create_order_saga"
 	"github.com/tkame123/ddd-sample/app/order_api/domain/service/create_order_saga/event_handler"
-	"github.com/tkame123/ddd-sample/lib/event_helper"
+	"github.com/tkame123/ddd-sample/proto/message"
 )
 
 type CreateOrderSagaContext struct {
 	strategy domain_event.EventHandler
-	event    event_helper.Event
+	mes      *message.Message
 }
 
-func NewCreateOrderSagaContext(ev event_helper.Event, saga *create_order_saga.CreateOrderSaga) (*CreateOrderSagaContext, error) {
-	if !event_handler.IsCreateOrderSagaEvent(ev.Name()) {
-		return nil, errors.New("unknown event by CreateOrderSagaContext")
+func NewCreateOrderSagaContext(mes *message.Message, saga *create_order_saga.CreateOrderSaga) (*CreateOrderSagaContext, error) {
+	if !event_handler.IsCreateOrderSagaEvent(mes.Subject.Type) {
+		return nil, fmt.Errorf("invalid event type: %s", mes.Subject.Type)
 	}
 
 	var strategy domain_event.EventHandler
-	switch ev.Name() {
-	case event_helper.EventName_OrderCreated:
+	switch mes.Subject.Type {
+	case message.Type_TYPE_EVENT_ORDER_CREATED:
 		strategy = event_handler.NewNextStepSagaWhenOrderCreatedHandler(saga)
-	case event_helper.EventName_OrderApproved:
+	case message.Type_TYPE_EVENT_ORDER_APPROVED:
 		strategy = event_handler.NewNextStepSagaWhenOrderApprovedHandler(saga)
-	case event_helper.EventName_OrderRejected:
+	case message.Type_TYPE_EVENT_ORDER_REJECTED:
 		strategy = event_handler.NewNextStepSagaWhenOrderRejectedHandler(saga)
-	case event_helper.EventName_TicketCreated:
+	case message.Type_TYPE_EVENT_TICKET_CREATED:
 		strategy = event_handler.NewNextStepSagaWhenTicketCreatedHandler(saga)
-	case event_helper.EventName_TicketCreationFailed:
+	case message.Type_TYPE_EVENT_TICKET_CREATION_FAILED:
 		strategy = event_handler.NewNextStepSagaWhenTicketCreationFailedHandler(saga)
-	case event_helper.EventName_TicketApproved:
+	case message.Type_TYPE_EVENT_TICKET_APPROVED:
 		strategy = event_handler.NewNextStepSagaWhenTicketApprovedHandler(saga)
-	case event_helper.EventName_TicketRejected:
+	case message.Type_TYPE_EVENT_TICKET_REJECTED:
 		strategy = event_handler.NewNextStepSagaWhenTicketRejectedHandler(saga)
-	case event_helper.EventName_CardAuthorized:
+	case message.Type_TYPE_EVENT_CARD_AUTHORIZED:
 		strategy = event_handler.NewNextStepSagaWhenCardAuthorizedHandler(saga)
-	case event_helper.EventName_CardAuthorizeFailed:
+	case message.Type_TYPE_EVENT_CARD_AUTHORIZATION_FAILED:
 		strategy = event_handler.NewNextStepSagaWhenCardAuthorizeFailedHandler(saga)
 
 	default:
 		return nil, errors.New("not implemented event by CreateOrderSagaContext")
 	}
 
-	return &CreateOrderSagaContext{strategy: strategy, event: ev}, nil
+	return &CreateOrderSagaContext{strategy: strategy, mes: mes}, nil
 }
 
 func (c *CreateOrderSagaContext) Handler(ctx context.Context) error {
-	return c.strategy.Handler(ctx, c.event)
+	return c.strategy.Handler(ctx, c.mes)
 }
