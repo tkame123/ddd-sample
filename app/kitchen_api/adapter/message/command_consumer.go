519217498d64b6e38c332ba883ddb260a24561f6
Takeshi Kameyama <tkame123@gmail.com>
2024-10-12 15:42:54 +0900

    add command consumer on kitchen API

---
+++ app/kitchen_api/adapter/message/command_consumer.go
@@ -0,0 +1,152 @@
+package message
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
+	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
+	"github.com/tkame123/ddd-sample/app/kitchen_api/di/provider"
+	"github.com/tkame123/ddd-sample/app/kitchen_api/domain/port/service"
+	"github.com/tkame123/ddd-sample/app/kitchen_api/domain/service/create_ticket/event_handler"
+	"github.com/tkame123/ddd-sample/lib/event"
+	"github.com/tkame123/ddd-sample/lib/sqs_consumer"
+	"log"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+)
+
+const (
+	maxMessages = 5
+	maxWorkers  = 3
+	messageChan = 10
+)
+
+type EventConsumer struct {
+	sqsClient *sqs.Client
+	queueUrl  string
+	svc       service.CreateTicket
+}
+
+func NewEventConsumer(
+	envCfg *provider.EnvConfig,
+	sqsClient *sqs.Client,
+	svc service.CreateTicket,
+) *EventConsumer {
+	return &EventConsumer{
+		sqsClient: sqsClient,
+		queueUrl:  envCfg.SqsUrlTicketCommand,
+		svc:       svc,
+	}
+}
+
+func (e *EventConsumer) Run() {
+	// コンテキストとキャンセル関数を作成
+	ctxPolling, ctxPollingCancel := context.WithCancel(context.Background())
+	ctxWorker, ctxWorkerCancel := context.WithCancel(context.Background())
+
+	// SQS コンシューマを作成
+	wgPolling := new(sync.WaitGroup)
+	wgPolling.Add(1)
+	consumer := sqs_consumer.NewSQSConsumer(e.sqsClient, e.queueUrl, wgPolling)
+
+	// メッセージをやり取りするチャネルを作成
+	messagesChan := make(chan *types.Message, messageChan) // バッファ付きのチャネル
+
+	// Poller を起動
+	go consumer.PollMessages(ctxPolling, maxMessages, messagesChan)
+
+	// ワーカーを並列に起動
+	wgWorker := new(sync.WaitGroup)
+	for i := 0; i < maxWorkers; i++ {
+		wgWorker.Add(1)
+		worker := sqs_consumer.NewWorker(i, wgWorker, e.workerHandler)
+		go worker.Start(ctxWorker, messagesChan)
+	}
+
+	// SIGINT (Ctrl+C) で停止
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	<-sigs
+	log.Println("Shutting down...")
+
+	// pollingを停止する
+	ctxPollingCancel()
+	wgPolling.Wait()
+
+	// workerを停止する
+	ctxWorkerCancel()
+	wgWorker.Wait()
+}
+
+func (e *EventConsumer) workerHandler(ctx context.Context, msg *types.Message) error {
+	ev, err := parseEvent(msg)
+	if err != nil {
+		return err
+	}
+
+	err = e.processEvent(ctx, ev)
+	if err != nil {
+		return err
+	}
+
+	err = e.deleteMessage(ctx, msg)
+	if err != nil {
+		// TODO Transactional Outbox Patternの導入までは通知して手動対応ってなるのだろうか。。。
+		log.Printf("failed to delete message %v", err)
+	}
+
+	return nil
+}
+
+func (e *EventConsumer) processEvent(ctx context.Context, ev event.Event) error {
+	handler, err := NewCreateTicketContext(ev, e.svc)
+	if err != nil {
+		return err
+	}
+	err = handler.Handler(ctx)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (e *EventConsumer) deleteMessage(ctx context.Context, msg *types.Message) error {
+	_, err := e.sqsClient.DeleteMessage(ctx, &sqs.DeleteMessageInput{
+		QueueUrl:      aws.String(e.queueUrl),
+		ReceiptHandle: msg.ReceiptHandle,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func parseEvent(msg *types.Message) (event.Event, error) {
+	type Body struct {
+		Message string `json:"message"`
+	}
+	var body Body
+	if err := json.Unmarshal([]byte(*msg.Body), &body); err != nil {
+		return nil, err
+	}
+	var message event.RawEvent
+	if err := json.Unmarshal([]byte(body.Message), &message); err != nil {
+		return nil, err
+	}
+
+	eventFac, err := event_handler.NewCreateTicketServiceEventFactory(message)
+	if err != nil {
+		return nil, err
+	}
+	domainEvent, err := eventFac.Event()
+	if err != nil {
+		return nil, err
+	}
+
+	return domainEvent, nil
+}
---
+++ app/kitchen_api/adapter/message/create_ticket_context.go
@@ -0,0 +1,46 @@
+package message
+
+import (
+	"context"
+	"errors"
+	"github.com/tkame123/ddd-sample/app/kitchen_api/domain/model"
+	"github.com/tkame123/ddd-sample/app/kitchen_api/domain/port/domain_event"
+	"github.com/tkame123/ddd-sample/app/kitchen_api/domain/port/service"
+	"github.com/tkame123/ddd-sample/app/kitchen_api/domain/service/create_ticket/event_handler"
+	"github.com/tkame123/ddd-sample/lib/event"
+)
+
+type CreateTicketContext struct {
+	strategy domain_event.EventHandler
+	event    event.Event
+	svc      service.CreateTicket
+}
+
+func NewCreateTicketContext(ev event.Event, svc service.CreateTicket) (*CreateTicketContext, error) {
+	if !event_handler.IsCreateTicketEvent(ev.Name()) {
+		return nil, errors.New("unknown event by CreateTicketService")
+	}
+
+	var strategy domain_event.EventHandler
+	switch ev.Name() {
+	case event.CommandName_TicketCreate:
+		v, ok := ev.(*model.TicketCreateCommand)
+		if !ok {
+			return nil, errors.New("invalid event")
+		}
+		strategy = event_handler.NewTicketCreateWhenTicketCreateHandler(v.OrderID, v.Items, svc)
+	case event.CommandName_TicketApprove:
+		strategy = event_handler.NewTicketApproveWhenTicketApproveHandler(ev.ID(), svc)
+	case event.CommandName_TicketReject:
+		strategy = event_handler.NewTicketRejectWhenTicketRejectHandler(ev.ID(), svc)
+
+	default:
+		return nil, errors.New("not implemented event by CreateTicketService")
+	}
+
+	return &CreateTicketContext{strategy: strategy, event: ev, svc: svc}, nil
+}
+
+func (c *CreateTicketContext) Handler(ctx context.Context) error {
+	return c.strategy.Handler(ctx, c.event)
+}
