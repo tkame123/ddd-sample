1e5e62882c0aaa4572f599242c23101c0b65e4ab
Takeshi Kameyama <tkame123@gmail.com>
2024-10-13 10:20:36 +0900

    fixed comment

--- app/kitchen_api/adapter/database/ticket.go
+++ app/kitchen_api/adapter/database/ticket.go
@@ -8,18 +8,18 @@ import (
 
 func (r repo) TicketFindOne(ctx context.Context, id model.TicketID) (*model.Ticket, error) {
 	//TODO implement me
-	log.Println("implement me")
+	log.Println("implement me: TicketFindOne")
 	return nil, nil
 }
 
 func (r repo) TicketFindOneByOrderID(ctx context.Context, id model.OrderID) (*model.Ticket, error) {
 	//TODO implement me
-	log.Println("implement me")
+	log.Println("implement me: TicketFindOneByOrderID")
 	return nil, nil
 }
 
 func (r repo) TicketSave(ctx context.Context, order *model.Ticket) error {
 	//TODO implement me
-	log.Println("implement me")
+	log.Println("implement me: TicketSave")
 	return nil
 }
