b68ccf86a8fdd51b4396edd7e661ffc803bcad6b
Takeshi Kameyama <tkame123@gmail.com>
2024-10-22 15:14:40 +0900

    refactor

--- lib/auth/policy.go
+++ lib/auth/policy.go
@@ -1,16 +1,10 @@
 package auth
 
 import (
-	"github.com/tkame123/ddd-sample/lib/metadata"
 	"strings"
 )
 
 type Permission string
-
-const (
-	OrdersRead  metadata.Permission = "read:orders"
-	OrdersWrite metadata.Permission = "write:orders"
-)
 
 func (p Permission) Sub() string {
 	result := strings.Split(string(p), ":")
